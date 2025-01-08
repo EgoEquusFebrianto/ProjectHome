6eead273bc9ac876d5c05013dd1a22c66be541e4
kudadiriFTO <jekalaguna1945@gmail.com>
2025-01-08 11:23:48 +0700

    this is first commit

---
+++ Home/Welcome.py
@@ -0,0 +1,26 @@
+from pyspark.sql import *
+from lib.logging import Log4j
+import os
+
+os.environ['SPARK_LOCAL_IP'] = "172.25.5.7"
+
+if __name__ == "__main__":
+    location = "-Dlog4j.configurationFile=file:/home/kudadiri/ProjectHome/Home/log4j.properties"
+    folder_log = "-Dspark.yarn.app.container.log.dir=app-logs"
+    file_log = "-Dlogfile.name=spark-app"
+
+    spark = (SparkSession.builder
+        .appName("Spark Example")
+        .config("spark.driver.extraJavaOptions", f"{location} {folder_log} {file_log}")
+        .master("local[2]")
+        .getOrCreate()
+    )
+    print(spark.sparkContext.uiWebUrl)
+    spark.sparkContext.setLogLevel("INFO")
+    logger = Log4j(spark)
+
+    logger.info("Starting Spark Program")
+    print("Hello World!")
+    logger.info("Spark Program Terminate")
+
+    #spark.stop()

d60e2c317f96e8c3763bd5dd18d76dd7e4e9a9c7
kudadiriFTO <jekalaguna1945@gmail.com>
2025-02-13 15:07:11 +0700

    commited

--- introduction_to_spark_app/EXP_GroupingAgg.py
+++ introduction_to_spark_app/EXP_GroupingAgg.py
@@ -4,12 +4,12 @@ from pyspark.sql.types import *
 import pyspark.sql.functions as f
 import os
 
-os.environ["SPARK_LOCAL_IP"] = "172.25.5.7"
+os.environ["SPARK_LOCAL_IP"] = "IP"
 
 if __name__ == "__main__":
     # konfigurasi ini saya sarankan dibuat di file spark-defaults.conf
-    location = "-Dlog4j.configurationFile=file:/home/kudadiri/anaconda3/envs/Home/lib/python3.10/site-packages/pyspark/conf/log4j2v2.properties"
-    folder_log = "-Dspark.yarn.app.container.log.dir=app-logs"
+    location = "-Dlog4j.configurationFile=file:/path/to/your/log4j<1 or 2>.properties"
+    folder_log = "-Dspark.yarn.app.container.log.dir=/path/to/your/app-logs"
     file_log = "-Dlogfile.name=AggregationDataFrame-app"
 
     spark = SparkSession.builder \
@@ -26,7 +26,7 @@ if __name__ == "__main__":
         .format("csv") \
         .option("header", True) \
         .option("inferSchema", True) \
-        .load("/mnt/d/data/spark/invoices.csv")
+        .load("data/spark/invoices.csv")
 
     # df.printSchema()
 
@@ -47,7 +47,7 @@ if __name__ == "__main__":
         .write \
         .format("parquet") \
         .mode("overwrite") \
-        .option("path", "/mnt/d/DataSinkResult/parquet/GroupingAgg/") \
+        .option("path", "DataSinkResult/parquet/GroupingAgg/") \
         .save()
 
     logger.info("Spark Terminate")

82c1b257162117e4572bc85cb7bad2f046a04e0c
kudadiriFTO <jekalaguna1945@gmail.com>
2025-03-11 22:23:30 +0700

    commited

--- lab/run.sh
+++
@@ -1,5 +0,0 @@
-spark-submit --master yarn --deploy-mode cluster \
-  --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=/home/kudadiri/anaconda3/envs/Home/bin/python \
-  --conf spark.yarn.appMasterEnv.PYSPARK_DRIVER_PYTHON=/home/kudadiri/anaconda3/envs/Home/bin/python \
-  --conf spark.yarn.executorEnv.PYSPARK_PYTHON=/home/kudadiri/anaconda3/envs/Home/bin/python \
-    main.py local 2025-02-19
